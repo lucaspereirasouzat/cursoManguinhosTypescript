08f916a035a14f2e7d1c4f219a671c072bfb24bc
lucas <lucas@telium.com.br>
2024-10-08 00:08:15 -0300

    change test strategy to use vitest mocks

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,12 +1,11 @@
-import {describe, it, expect, vitest} from 'vitest'
+import {describe, it, expect} from 'vitest'
 import { AuthenticationError } from '@/domain/errors/authentication'
 import { FacebookAuthenticationService } from '@/data/services'
-
+import {mock} from "vitest-mock-extended"
+import { LoadFacebookUserApi } from '@/data/contracts/apis';
 describe('FacebookAuthenticationService', () => {
     it('load call LoadFacebookUserApi with correct params', async () => {
-        const loadFacebookUserApi = {
-            loadUser: vitest.fn()
-        }
+        const loadFacebookUserApi = mock<LoadFacebookUserApi>()
         const sut = new FacebookAuthenticationService(loadFacebookUserApi)
         await sut.perform({ token: 'any_token' })
 
@@ -16,9 +15,7 @@ describe('FacebookAuthenticationService', () => {
         expect(loadFacebookUserApi.loadUser).toBeCalledTimes(1)
     })
      it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
-        const loadFacebookUserApi = {
-            loadUser: vitest.fn()
-        }
+        const loadFacebookUserApi = mock<LoadFacebookUserApi>()
         loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
         const sut = new FacebookAuthenticationService(loadFacebookUserApi)
         const result = await sut.perform({ token: 'any_token' })
