83f50acb6999ccdfbe612334ebdfd44232be5e54
lucas <lucas@telium.com.br>
2024-11-02 17:07:55 -0300

    Refactor FacebookAuthenticationService tests to handle error cases

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -117,4 +117,38 @@ describe("FacebookAuthenticationService", () => {
 
     expect(authResult).toEqual(new AccessToken('any_generated_token'));
   });
+
+  it('should rethrow if LoadFacebookUserApi throws', async () => {
+    facebookApi.loadUser.mockRejectedValueOnce(new Error('facebook_error'));
+
+    const promise = sut.perform({ token });
+
+    await expect(promise).rejects.toThrow(new Error('facebook_error'));
+  })
+
+  it('should rethrow if LoadUserAccountRepository throws', async () => {
+    userAccountRepo.load.mockRejectedValueOnce(new Error('load_error'));
+
+    const promise = sut.perform({ token });
+
+    await expect(promise).rejects.toThrow(new Error('load_error'));
+  })
+
+  it('should rethrow if SaveFacebookAccountRepository throws', async () => {
+    userAccountRepo.saveWithFacebook.mockRejectedValueOnce(new Error('save_error'));
+
+    const promise = sut.perform({ token });
+
+    await expect(promise).rejects.toThrow(new Error('save_error'));
+  })
+
+  it('should rethrow if TokenGenerator throws', async () => {
+    crypto.generateToken.mockRejectedValueOnce(new Error('token_error'));
+
+    const promise = sut.perform({ token });
+
+    await expect(promise).rejects.toThrow(new Error('token_error'));
+  })
+
+  
 });
