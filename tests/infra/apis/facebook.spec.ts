50f8146f0560f633408c08dea0296bcfd00e32ed
lucas <lucas@telium.com.br>
2024-11-03 03:38:55 -0300

    Refactor FacebookApi tests to include loading app token and update dependencies

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,54 +1,65 @@
 import { LoadFacebookUserApi } from "@/data/contracts/apis";
-import { describe, expect, it } from "vitest";
-import { mock } from "vitest-mock-extended";
+import { beforeAll, beforeEach, describe, expect, it } from "vitest";
+import { mock, MockProxy } from "vitest-mock-extended";
 
 class FacebookApi {
-    private readonly baseUrl = 'https://graph.facebook.com'
-    constructor(
-        private readonly httpClient: HttpGetClient, 
-        private readonly clientId: string, 
-        private readonly clientSecret: string
-    ) { }
-    async loadUser(params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
-        await this.httpClient.get({
-            url: `${this.baseUrl}/oauth/access_token`,
-            params: {
-                client_id: this.clientId,
-                client_secret: this.clientSecret,
-                grant_type: 'client_credentials'
-            }
-        })
-    }
-}     
+  private readonly baseUrl = "https://graph.facebook.com";
+  constructor(
+    private readonly httpClient: HttpGetClient,
+    private readonly clientId: string,
+    private readonly clientSecret: string
+  ) {}
+  async loadUser(
+    params: LoadFacebookUserApi.Params
+  ): Promise<LoadFacebookUserApi.Result> {
+    await this.httpClient.get({
+      url: `${this.baseUrl}/oauth/access_token`,
+      params: {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        grant_type: "client_credentials",
+      },
+    });
+  }
+}
 
 interface HttpGetClient {
-    get: (params: HttpGetClient.Params) => Promise<void>
+  get: (params: HttpGetClient.Params) => Promise<void>;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 namespace HttpGetClient {
-    export type Params = {
-        url: string
-        params: object
-    }
+  export type Params = {
+    url: string;
+    params: object;
+  };
 }
 
-describe('FacebookApi', () => {
-    const clientId = 'any_client_id'
-    const clientSecret = 'any_client_secret'
-    it("should get app token", async () => {
-        const httpClient = mock<HttpGetClient>()
-        const sut = new FacebookApi(httpClient, clientId, clientSecret)
+describe("FacebookApi", () => {
+  let clientId: string;
+  let clientSecret: string;
+  let sut: FacebookApi;
+  let httpClient: MockProxy<HttpGetClient>;
 
-        await sut.loadUser({ token: 'any_token'});
+  beforeAll(() => {
+    clientId = "any_client_id";
+    clientSecret = "any_client_secret";
+    httpClient = mock();
+  });
 
-        expect(httpClient.get).toBeCalledWith({
-            url: 'https://graph.facebook.com/oauth/access_token',
-            params: {
-                client_id: clientId,
-                client_secret: clientSecret,
-                grant_type: 'client_credentials'
-            }
-        });
-    })
+  beforeEach(() => {
+    sut = new FacebookApi(httpClient, clientId, clientSecret);
+  });
+  it("should get app token", async () => {
+    await sut.loadUser({ token: "any_token" });
+
+    expect(httpClient.get).toBeCalledWith({
+      url: "https://graph.facebook.com/oauth/access_token",
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: "client_credentials",
+      },
+    });
+  });
 });
