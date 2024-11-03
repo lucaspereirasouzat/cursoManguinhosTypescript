63d6aa09def24fd7e69838e9293cf1379ed484f2
lucas <lucas@telium.com.br>
2024-11-03 03:34:53 -0300

    Refactor FacebookApi tests to include loading app token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -4,13 +4,17 @@ import { mock } from "vitest-mock-extended";
 
 class FacebookApi {
     private readonly baseUrl = 'https://graph.facebook.com'
-    constructor(private readonly httpClient: HttpGetClient) { }
+    constructor(
+        private readonly httpClient: HttpGetClient, 
+        private readonly clientId: string, 
+        private readonly clientSecret: string
+    ) { }
     async loadUser(params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
         await this.httpClient.get({
             url: `${this.baseUrl}/oauth/access_token`,
             params: {
-                client_id: 'any_client_id',
-                client_secret: 'any_client_secret',
+                client_id: this.clientId,
+                client_secret: this.clientSecret,
                 grant_type: 'client_credentials'
             }
         })
@@ -30,17 +34,19 @@ namespace HttpGetClient {
 }
 
 describe('FacebookApi', () => {
+    const clientId = 'any_client_id'
+    const clientSecret = 'any_client_secret'
     it("should get app token", async () => {
         const httpClient = mock<HttpGetClient>()
-        const sut = new FacebookApi(httpClient)
+        const sut = new FacebookApi(httpClient, clientId, clientSecret)
 
         await sut.loadUser({ token: 'any_token'});
 
         expect(httpClient.get).toBeCalledWith({
             url: 'https://graph.facebook.com/oauth/access_token',
             params: {
-                client_id: 'any_client_id',
-                client_secret: 'any_client_secret',
+                client_id: clientId,
+                client_secret: clientSecret,
                 grant_type: 'client_credentials'
             }
         });
